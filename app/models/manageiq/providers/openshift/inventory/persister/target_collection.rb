32bd54d5a3a53e299a044e9b7a38d9edb49efaa3
Ladislav Smola <lsmola@redhat.com>
2017-10-19 10:41:43 +0200

    Fix rubocop issues
    Fix rubocop issues

--- app/models/manageiq/providers/openshift/inventory/persister/target_collection.rb
+++ app/models/manageiq/providers/openshift/inventory/persister/target_collection.rb
@@ -13,7 +13,7 @@ class ManageIQ::Providers::Openshift::Inventory::Persister::TargetCollection < M
     settings_options.merge(
       :targeted => targeted,
       :complete => false, # For now, we want to a only create and update elements using watches data, delete events could
-                          # probably set finished_at and deleted on dates, as an update based disconnect_inv.
+      # probably set finished_at and deleted on dates, as an update based disconnect_inv.
       :strategy => :local_db_find_references, # By default no IC will be saved
     )
   end
