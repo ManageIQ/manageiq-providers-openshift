1e319cbd03bfd8ff5eddc3cdf53f7c7cf07efa43
Martin Slemr <mslemr@redhat.com>
2018-07-03 17:17:42 +0200

    InventoryCollection Builder
    Persister's add_collection() interface

--- app/models/manageiq/providers/openshift/inventory/persister/container_manager.rb
+++ app/models/manageiq/providers/openshift/inventory/persister/container_manager.rb
@@ -1,13 +1,34 @@
 class ManageIQ::Providers::Openshift::Inventory::Persister::ContainerManager < ManageIQ::Providers::Openshift::Inventory::Persister
-  include ManageIQ::Providers::Kubernetes::ContainerManager::InventoryCollections
+  include ManageIQ::Providers::Kubernetes::Inventory::Persister::Definitions::ContainerCollections
 
   def initialize_inventory_collections
     super
+
+    initialize_container_inventory_collections
+
     # get_container_images=false mode is a stopgap to speed up refresh by reducing functionality.
     # Skipping these InventoryCollections (instead of returning empty ones)
     # to at least retain existing metadata if it was true and is now false.
     if options.get_container_images
-      initialize_custom_attributes_collections(@collections[:container_images], %w(labels docker_labels))
+      add_custom_attributes(:container_images, %w(labels docker_labels))
     end
+  end
+
+  protected
+
+  def targeted?
+    false
+  end
+
+  def strategy
+    nil
+  end
+
+  def shared_options
+    {
+      :strategy => strategy,
+      :targeted => targeted?,
+      :parent   => manager.presence
+    }
   end
 end
