e6a65c346f5a74b7a5ae34df52b1d78e44f6c84d
Adam Grare <agrare@redhat.com>
2018-08-10 13:26:55 -0400

    Parse templates

--- app/models/manageiq/providers/openshift/inventory/parser/watches.rb
+++ app/models/manageiq/providers/openshift/inventory/parser/watches.rb
@@ -9,8 +9,25 @@ class ManageIQ::Providers::Openshift::Inventory::Parser::Watches < ManageIQ::Pro
   private
 
   def parse_images(image_notices)
+    return if image_notices.blank?
   end
 
   def parse_templates(template_notices)
+    return if template_notices.blank?
+
+    template_notices.each do |template_notice|
+      next if template_notice.action == "DELETE"
+
+      template = template_notice.object
+      persister.container_templates.build(
+        :ems_ref          => template.metadata.uid,
+        :name             => template.metadata.name,
+        :namespace        => template.metadata.namespace,
+        :ems_created_on   => template.metadata.creationTimestamp,
+        :resource_version => template.metadata.resourceVersion,
+        :objects          => template.objects.to_a.collect(&:to_h),
+        :object_labels    => template.labels.to_h,
+      )
+    end
   end
 end
