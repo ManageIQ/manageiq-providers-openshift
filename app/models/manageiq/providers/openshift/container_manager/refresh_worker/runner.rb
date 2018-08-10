4739e6df8f7740981d218356bba2bc22b09481c2
Adam Grare <agrare@redhat.com>
2018-08-10 09:29:43 -0400

    Include StreamingRefreshMixin

--- app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
@@ -1,2 +1,3 @@
 class ManageIQ::Providers::Openshift::ContainerManager::RefreshWorker::Runner < ManageIQ::Providers::BaseManager::RefreshWorker::Runner
+  include ManageIQ::Providers::Kubernetes::ContainerManager::StreamingRefreshMixin
 end
