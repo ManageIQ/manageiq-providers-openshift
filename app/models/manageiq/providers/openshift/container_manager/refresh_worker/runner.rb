fda47403d016261108cc9d114d788fcec737055d
Adam Grare <agrare@redhat.com>
2018-08-10 09:38:19 -0400

    Add openshift entities to watch streams

--- app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
@@ -1,3 +1,21 @@
 class ManageIQ::Providers::Openshift::ContainerManager::RefreshWorker::Runner < ManageIQ::Providers::BaseManager::RefreshWorker::Runner
   include ManageIQ::Providers::Kubernetes::ContainerManager::StreamingRefreshMixin
+
+  def start_watches
+    watch_streams = super
+
+    openshift_connection = ems.connect
+    openshift_entities.each do |entity|
+      watch_streams[entity] = start_watch(openshift_connection, entity)
+    end
+
+    watch_streams
+  end
+
+  def openshift_entities
+    %w(
+      templates
+      images
+    )
+  end
 end
