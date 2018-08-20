aab5e3178363cf821b6eeaa711ddb14fbb81e79b
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:43:04 +0200

    Add plugin display name

--- lib/manageiq/providers/openshift/engine.rb
+++ lib/manageiq/providers/openshift/engine.rb
@@ -3,6 +3,10 @@ module ManageIQ
     module Openshift
       class Engine < ::Rails::Engine
         isolate_namespace ManageIQ::Providers::Openshift
+
+        def self.plugin_name
+          _('OpenShift Provider')
+        end
       end
     end
   end
