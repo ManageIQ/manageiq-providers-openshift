96806dc6ea3f24343efc6b64846e5ddb678ccb3e
Gregg Tanzillo <gtanzill@redhat.com>
2020-03-23 15:23:38 -0400

    Merge pull request #159 from agrare/cleanup_openshift_refresh
    Cleanup remnants from topology service catalog refresh
    
    (cherry picked from commit ae9f7722e6b25c609f6a21d2b346ca2f8cda858e)

--- app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
@@ -1,21 +1,2 @@
 class ManageIQ::Providers::Openshift::ContainerManager::RefreshWorker::Runner < ManageIQ::Providers::BaseManager::RefreshWorker::Runner
-  include ManageIQ::Providers::Kubernetes::ContainerManager::StreamingRefreshMixin
-
-  def connection_for_entity(entity_type)
-    super || (openshift_connection if openshift_entity_types.include?(entity_type))
-  end
-
-  def openshift_connection
-    @openshift_connection ||= connect("openshift")
-  end
-
-  def openshift_entity_types
-    %w(
-      templates
-    )
-  end
-
-  def all_entity_types
-    super + openshift_entity_types
-  end
 end
--- app/models/manageiq/providers/openshift/inventory/collector.rb
+++ app/models/manageiq/providers/openshift/inventory/collector.rb
@@ -1,4 +1,3 @@
 class ManageIQ::Providers::Openshift::Inventory::Collector < ManageIQ::Providers::Kubernetes::Inventory::Collector
   require_nested :TargetCollection
-  require_nested :Watches
 end
