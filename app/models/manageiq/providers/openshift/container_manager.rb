2763c4bba3672206da957e0cd683045fb33dddfb
Jason Frey <jfrey@redhat.com>
2020-03-26 15:56:52 -0400

    Merge pull request #161 from agrare/refactor_graph_refresh
    Refactor graph refresh to match current standards
    
    (cherry picked from commit 50f2ba0964cc569248a582308c3ec6ad1b7568f6)

--- app/models/manageiq/providers/openshift/container_manager.rb
+++ app/models/manageiq/providers/openshift/container_manager.rb
@@ -7,7 +7,6 @@ class ManageIQ::Providers::Openshift::ContainerManager < ManageIQ::Providers::Co
   require_nested :EventParser
   require_nested :MetricsCollectorWorker
   require_nested :OrchestrationStack
-  require_nested :RefreshParser
   require_nested :RefreshWorker
   require_nested :Refresher
   require_nested :Options
--- app/models/manageiq/providers/openshift/container_manager/refresher.rb
+++ app/models/manageiq/providers/openshift/container_manager/refresher.rb
@@ -1,37 +1,6 @@
 module ManageIQ::Providers
   module Openshift
     class ContainerManager::Refresher < ManageIQ::Providers::BaseManager::Refresher
-      include ManageIQ::Providers::Kubernetes::ContainerManager::RefresherMixin
-
-      KUBERNETES_EMS_TYPE = ManageIQ::Providers::Kubernetes::ContainerManager.ems_type
-
-      OPENSHIFT_ENTITIES = [
-        {:name => 'routes'}, {:name => 'projects'},
-        {:name => 'build_configs'}, {:name => 'builds'}, {:name => 'templates'}
-      ]
-
-      def refresh_parser_class
-        ManageIQ::Providers::Openshift::ContainerManager::RefreshParser
-      end
-
-      def all_entities
-        OPENSHIFT_ENTITIES + KUBERNETES_ENTITIES + [{:name => 'images'}]
-      end
-
-      def collect_full_inventory(ems)
-        request_entities = OPENSHIFT_ENTITIES.dup
-        request_entities << {:name => 'images'} if refresher_options.get_container_images
-
-        kube_inventory = ems.with_provider_connection(:service => KUBERNETES_EMS_TYPE) do |kubeclient|
-          fetch_entities(kubeclient, KUBERNETES_ENTITIES)
-        end
-        openshift_inventory = ems.with_provider_connection do |openshift_client|
-          fetch_entities(openshift_client, request_entities)
-        end
-
-        inventory = openshift_inventory.merge(kube_inventory)
-        inventory
-      end
     end
   end
 end
