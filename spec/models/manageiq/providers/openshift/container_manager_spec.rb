ca5548f9694da485cbbdb24be9e4312207c48a2f
Jason Frey <fryguy9@gmail.com>
2020-04-09 13:39:16 -0400

    Merge pull request #163 from agrare/make_catalog_types_pluggable
    Make Provider Catalog Types Pluggable
    
    (cherry picked from commit 29370d2b65aa8162d2b297a55b41568876407738)

--- spec/models/manageiq/providers/openshift/container_manager_spec.rb
+++ spec/models/manageiq/providers/openshift/container_manager_spec.rb
@@ -1,6 +1,6 @@
 describe ManageIQ::Providers::Openshift::ContainerManager do
-  it "#supported_catalog_types" do
+  it "#catalog_types" do
     ems = FactoryBot.create(:ems_openshift)
-    expect(ems.supported_catalog_types).to match_array(%w(generic_container_template))
+    expect(ems.catalog_types).to include("generic_container_template")
   end
 end
