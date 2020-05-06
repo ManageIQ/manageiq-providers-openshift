feccb072cbdca75318acdeb857b3c5ca75c67acc
Adam Grare <agrare@redhat.com>
2020-05-06 14:45:15 -0400

    Only subscribe to image watches if storing unused images

--- app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/openshift/container_manager/refresh_worker/runner.rb
@@ -4,6 +4,9 @@ class ManageIQ::Providers::Openshift::ContainerManager::RefreshWorker::Runner < 
   end
 
   def openshift_entity_types
-    %w[projects routes build_configs builds templates images]
+    %w[projects routes build_configs builds templates].tap do |types|
+      # Only subscribe to Image watches if we're getting and storing OpenShift Images
+      types << "images" if refresher_options.get_container_images && !refresher_options.store_unused_images
+    end
   end
 end
