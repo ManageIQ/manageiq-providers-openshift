b237ac72be93ae88c71c7c90f64c0ff4f6e6e2d3
Jason Frey <fryguy9@gmail.com>
2021-04-26 15:33:30 -0400

    Cleanup module nesting

--- app/models/manageiq/providers/openshift/monitoring_manager.rb
+++ app/models/manageiq/providers/openshift/monitoring_manager.rb
@@ -1,24 +1,22 @@
 ManageIQ::Providers::Kubernetes::MonitoringManager.include(ActsAsStiLeafClass)
 
-module ManageIQ::Providers
-  class Openshift::MonitoringManager < ManageIQ::Providers::Kubernetes::MonitoringManager
-    require_nested :EventCatcher
+class ManageIQ::Providers::Openshift::MonitoringManager < ManageIQ::Providers::Kubernetes::MonitoringManager
+  require_nested :EventCatcher
 
-    belongs_to :parent_manager,
-               :foreign_key => :parent_ems_id,
-               :class_name  => "ManageIQ::Providers::Openshift::ContainerManager",
-               :inverse_of  => :monitoring_manager
+  belongs_to :parent_manager,
+             :foreign_key => :parent_ems_id,
+             :class_name  => "ManageIQ::Providers::Openshift::ContainerManager",
+             :inverse_of  => :monitoring_manager
 
-    def self.ems_type
-      @ems_type ||= "openshift_monitor".freeze
-    end
+  def self.ems_type
+    @ems_type ||= "openshift_monitor".freeze
+  end
 
-    def self.description
-      @description ||= "Openshift Monitor".freeze
-    end
+  def self.description
+    @description ||= "Openshift Monitor".freeze
+  end
 
-    def self.event_monitor_class
-      ManageIQ::Providers::Openshift::MonitoringManager::EventCatcher
-    end
+  def self.event_monitor_class
+    ManageIQ::Providers::Openshift::MonitoringManager::EventCatcher
   end
 end
