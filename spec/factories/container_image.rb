a431b2e6db5223d34b831dc60e05a6556734af94
Adam Grare <adam@grare.com>
2022-04-26 16:05:54 -0400

    Update container_image_spec

--- spec/factories/container_image.rb
+++ spec/factories/container_image.rb
@@ -2,4 +2,6 @@ FactoryBot.define do
   factory :openshift_container_image, :class => "ManageIQ::Providers::Openshift::ContainerManager::ContainerImage" do
     sequence(:name) { |n| "openshift_container_image_#{seq_padded_for_sorting(n)}" }
   end
+
+  factory :openshift_managed_container_image, :class => "ManageIQ::Providers::Openshift::ContainerManager::ManagedContainerImage", :parent => :openshift_container_image
 end
