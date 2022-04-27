94ed6057e0f9721a9fe2e7fd642e76ab5d408f5d
Adam Grare <adam@grare.com>
2022-04-27 12:23:47 -0400

    Include all ContainerImage descendants for disconnect_inv

--- app/models/manageiq/providers/openshift/container_manager/container_image.rb
+++ app/models/manageiq/providers/openshift/container_manager/container_image.rb
@@ -2,4 +2,9 @@ ManageIQ::Providers::Kubernetes::ContainerManager::ContainerImage.include(ActsAs
 
 class ManageIQ::Providers::Openshift::ContainerManager::ContainerImage < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerImage
   supports_not :capture
+
+  def self.disconnect_inv(ids)
+    _log.info "Disconnecting Images [#{ids}]"
+    base_class.where(:id => ids).update_all(:container_image_registry_id => nil, :deleted_on => Time.now.utc)
+  end
 end
