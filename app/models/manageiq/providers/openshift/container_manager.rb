612eced99372f57bf3e74ea198b2fb92703b7228
Adam Grare <adam@grare.com>
2023-03-22 15:46:21 -0400

    Remove /oapi fallback from connect_client

--- app/models/manageiq/providers/openshift/container_manager.rb
+++ app/models/manageiq/providers/openshift/container_manager.rb
@@ -100,15 +100,16 @@ class ManageIQ::Providers::Openshift::ContainerManager < ManageIQ::Providers::Ku
     if version
       @clients[api_version] ||= connect(:service => 'kubernetes', :version => version, :path => '/apis/' + api)
     else
+      kubernetes_client_key = File.join("/api", api_version)
+      @clients[kubernetes_client_key] ||= connect(:service => 'kubernetes', :version => api_version)
+
       # If we're given an OpenShift object lookup its v4 API Group
       api_group = self.class.api_group_for_kind(kind)
-      path      = api_group ? "/apps/#{api_group}" : "/oapi"
+      if api_group
+        openshift_client_key  = File.join(path, "/apps/#{api_group}")
+        @clients[openshift_client_key] ||= connect(:api_group => api_group, :version => api_version)
+      end
 
-      openshift_client_key  = File.join(path, api_version)
-      kubernetes_client_key = File.join("/api", api_version)
-
-      @clients[openshift_client_key] ||= connect(:api_group => api_group, :version => api_version)
-      @clients[kubernetes_client_key] ||= connect(:service => 'kubernetes', :version => api_version)
       @clients[openshift_client_key].respond_to?(method_name) ? @clients[openshift_client_key] : @clients[kubernetes_client_key]
     end
   end
