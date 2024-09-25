fb9cfa936e192f88d152b56aa5138246447e2a27
Adam Grare <adam@grare.com>
2024-09-25 12:55:23 -0400

    Fix RefreshWorker partial refresh error

--- app/models/manageiq/providers/openshift/infra_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/openshift/infra_manager/refresh_worker/runner.rb
@@ -6,4 +6,16 @@ class ManageIQ::Providers::Openshift::InfraManager::RefreshWorker::Runner < Mana
   def provider_class
     ManageIQ::Providers::Openshift
   end
+
+  def collector_class
+    ManageIQ::Providers::Openshift::Inventory::Collector::InfraManager
+  end
+
+  def partial_refresh_parser_class
+    ManageIQ::Providers::Openshift::Inventory::Parser::InfraManager::PartialRefresh
+  end
+
+  def persister_class
+    ManageIQ::Providers::Openshift::Inventory::Persister::InfraManager
+  end
 end
