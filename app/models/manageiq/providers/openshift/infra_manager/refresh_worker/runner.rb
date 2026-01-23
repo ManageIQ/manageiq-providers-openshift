a00466140723a8c672d8ffcfe8a47558ef211272
Adam Grare <adam@grare.com>
2026-01-23 14:08:38 -0500

    Move VirtualMachineTemplate handling to OpenShift

--- app/models/manageiq/providers/openshift/infra_manager/refresh_worker/runner.rb
+++ app/models/manageiq/providers/openshift/infra_manager/refresh_worker/runner.rb
@@ -18,4 +18,60 @@ class ManageIQ::Providers::Openshift::InfraManager::RefreshWorker::Runner < Mana
   def persister_class
     ManageIQ::Providers::Openshift::Inventory::Persister::InfraManager
   end
+
+  #
+  # Performs a full refresh.
+  #
+  def full_refresh
+    # Create and populate the collector, persister and parser
+    # and parse inventories
+    inventory = provider_class::Inventory.build(manager, nil)
+    collector = inventory.collector
+    persister = inventory.parse
+
+    # execute persist:
+    persister&.persist!
+
+    # Update the memory:
+    memory.add_list_version(:nodes, collector.nodes.resourceVersion)
+    memory.add_list_version(:vms, collector.vms.resourceVersion)
+    memory.add_list_version(:vm_instances, collector.vm_instances.resourceVersion)
+    memory.add_list_version(:templates, collector.templates.resourceVersion)
+    memory.add_list_version(:instance_types, collector.instance_types.resourceVersion)
+
+    manager.update(:last_refresh_error => nil, :last_refresh_date => Time.now.utc)
+  rescue StandardError => error
+    _log.error('Full refresh failed.')
+    _log.log_backtrace(error)
+    manager.update(:last_refresh_error => error.to_s, :last_refresh_date => Time.now.utc)
+  end
+
+  #
+  # Start watches
+  #
+  def start_watches
+    # This flag will be used to tell the threads to get out of their loops:
+    @finish = Concurrent::AtomicBoolean.new(false)
+
+    # Create the watches:
+    @watches = []
+    @watches << @manager.kubeclient.watch_nodes(:resource_version => memory.get_list_version(:nodes))
+    @watches << @manager.kubeclient("kubevirt.io/v1").watch_virtual_machines(:resource_version => memory.get_list_version(:vms))
+    @watches << @manager.kubeclient("kubevirt.io/v1").watch_virtual_machine_instances(:resource_version => memory.get_list_version(:vm_instances))
+    @watches << @manager.kubeclient("template.openshift.io/v1").watch_templates(:resource_version => memory.get_list_version(:templates))
+    @watches << @manager.kubeclient("instancetype.kubevirt.io/v1beta1").watch_virtual_machine_cluster_instancetypes(:resource_version => memory.get_list_version(:instance_types))
+
+    # Create the threads that run the watches and put the notices in the queue:
+    @watchers = []
+    @watches.each do |watch|
+      thread = Thread.new do
+        until @finish.value
+          watch.each do |notice|
+            @queue.push(notice)
+          end
+        end
+      end
+      @watchers << thread
+    end
+  end
 end
--- app/models/manageiq/providers/openshift/inventory/persister/infra_manager.rb
+++ app/models/manageiq/providers/openshift/inventory/persister/infra_manager.rb
@@ -1,2 +1,16 @@
 class ManageIQ::Providers::Openshift::Inventory::Persister::InfraManager < ManageIQ::Providers::Kubevirt::Inventory::Persister
+  def template_collection(targeted: false, ids: [])
+    add_collection(infra, :miq_templates) do |builder|
+      builder.add_properties(
+        :targeted                     => targeted,
+        :manager_uuids                => ids,
+        :parent_inventory_collections => %i(vms)
+      )
+
+      builder.add_default_values(
+        :type   => "#{manager.class}::Template",
+        :vendor => manager.class.vendor
+      )
+    end
+  end
 end
