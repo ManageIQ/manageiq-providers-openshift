66fed1c7344833cf838f667d272b23e2936d0c44
ishaaq-48 <ishaaq.sr@xaasio.com>
2026-07-24 19:20:31 +0530

    removed oc based console access

--- app/models/manageiq/providers/openshift/container_manager/container_group.rb
+++ app/models/manageiq/providers/openshift/container_manager/container_group.rb
@@ -1,9 +1,4 @@
 ManageIQ::Providers::Kubernetes::ContainerManager::ContainerGroup.include(ActsAsStiLeafClass)
 
 class ManageIQ::Providers::Openshift::ContainerManager::ContainerGroup < ManageIQ::Providers::Kubernetes::ContainerManager::ContainerGroup
-  private
-
-  def terminal_binary
-    "oc"
-  end
 end
